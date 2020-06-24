534587dc3b2df4f38956b88f042baf044fe8acae
Dannis <pengtaoklx@163.com>
2020-06-24 09:17:08 +0800

    Add files via upload

--- segmentation_models_pytorch/utils/losses.py
+++ segmentation_models_pytorch/utils/losses.py
@@ -7,7 +7,7 @@ from  .base import Activation
 
 class JaccardLoss(base.Loss):
 
-    def __init__(self, eps=1., activation=None, ignore_channels=None, **kwargs):
+    def __init__(self, eps=1., activation='sigmoid', ignore_channels=None, **kwargs):
         super().__init__(**kwargs)
         self.eps = eps
         self.activation = Activation(activation)
@@ -65,3 +65,27 @@ class BCELoss(nn.BCELoss, base.Loss):
 
 class BCEWithLogitsLoss(nn.BCEWithLogitsLoss, base.Loss):
     pass
+	
+class BCEJaccardLoss(JaccardLoss):
+    __name__ = 'bce_jaccard_loss'
+
+    def __init__(self, eps=1e-7, activation='sigmoid'):
+        super().__init__(eps=eps, activation=activation)
+        self.bce = nn.BCEWithLogitsLoss(reduction='mean')
+
+    def forward(self, y_pr, y_gt):
+        jaccard = super().forward(y_pr, y_gt)
+        bce = self.bce(y_pr, y_gt)
+        return jaccard + bce
+        
+class BCEDiceLoss(DiceLoss):
+    __name__ = 'bce_dice_loss'
+
+    def __init__(self, eps=1e-7, activation='sigmoid'):
+        super().__init__(eps=eps, beta=1.0, activation=activation)
+        self.bce = nn.BCEWithLogitsLoss(reduction='mean')
+
+    def forward(self, y_pr, y_gt):
+        dice = super().forward(y_pr, y_gt)
+        bce = self.bce(y_pr, y_gt)
+        return dice + bce
